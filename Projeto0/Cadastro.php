7279669ee0864dd70ba14d1e7b5fb886bdd06e7b
tbernardina <trdallabernardina@gmail.com>
2024-08-19 06:42:23 -0300

    Update Cadastro.php

--- Projeto0/Cadastro.php
+++ Projeto0/Cadastro.php
@@ -1,23 +1,22 @@
 
 <?php
 include('conexao.php');
-$mensagem = "";
 // Inserir usuário no banco de dados
-    if($_SERVER["REQUEST_METHOD"] == "POST") {
-        $nickname = $_POST['nickname'];
-        $nome = $_POST['nome'];
-        $sobrenome = $_POST['sobrenome'];
-        $senha = $_POST['senha'];
+//     if($_SERVER["REQUEST_METHOD"] == "POST") {
+//         $nickname = $_POST['nickname'];
+//         $nome = $_POST['nome'];
+//         $sobrenome = $_POST['sobrenome'];
+//         $senha = $_POST['senha'];
 
-        $insert = "INSERT INTO USUARIO (NOME, SOBRENOME, SENHA, NICKNAME) VALUES ('$nome', '$sobrenome', '$senha', '$nickname')";
+//         $insert = "INSERT INTO USUARIO (NOME, SOBRENOME, SENHA, NICKNAME) VALUES ('$nome', '$sobrenome', '$senha', '$nickname')";
 
-        if ($conn->query($insert) === TRUE) {
-            $mensagem = "Usuário cadastrado com sucesso!";
-        } else {
-            $mensagem = "Erro ao cadastrar usuário: " . $conn->error;
-        }
-    }
-$conn->close();
+//         if ($conn->query($insert) === TRUE) {
+//             $mensagem = "Usuário cadastrado com sucesso!";
+//         } else {
+//             $mensagem = "Erro ao cadastrar usuário: " . $conn->error;
+//         }
+//     }
+// $conn->close();
 ?>
 
 <!DOCTYPE html>
