b3b97fd774360a2b195b45e43cf958c3fb585dcb
tbernardina <trdallabernardina@gmail.com>
2024-08-19 06:41:53 -0300

    Update ProcessarCadastro.php

--- Projeto0/ProcessarCadastro.php
+++ Projeto0/ProcessarCadastro.php
@@ -1,1 +1,21 @@
+<?php
+include('conexao.php');
+$mensagem = "";
+// Inserir usuário no banco de dados
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $nickname = $_POST['nickname'];
+        $nome = $_POST['nome'];
+        $sobrenome = $_POST['sobrenome'];
+        $senha = $_POST['senha'];
 
+        $insert = "INSERT INTO USUARIO (NOME, SOBRENOME, SENHA, NICKNAME) VALUES ('$nome', '$sobrenome', '$senha', '$nickname')";
+
+        if ($conn->query($insert) === TRUE) {
+            $mensagem = "Usuário cadastrado com sucesso!";
+        } else {
+            $mensagem = "Erro ao cadastrar usuário: " . $conn->error;
+        }
+    }
+header('Location: Cadastro.php?mensagem=' $mensagem);
+exit();
+$conn->close();
