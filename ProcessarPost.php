dbc484d2cf26ed939de3bada89c459b8ff81e445
Thiago Reis Dalla Bernardina <trdallabernardina@gmail.com>
2024-08-20 12:56:37 -0300

    Implementação do processo com imagens

--- ProcessarPost.php
+++ ProcessarPost.php
@@ -1,24 +1,35 @@
-<?
+<?php
 include("conexao.php");
+session_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $titulo = $_POST['titulo'];
     $conteudo = $_POST['conteudo'];
     $autor = $_SESSION['id'];
-    $anexos = $_POST['anexo'];
+    $anexos = $_FILES['anexo'];
 
-    // Verifica se tanto o título quanto o conteúdo não estão vazios
-    // e faz a inserção do post ao banco de dados
+    $imagem = explode('.', $anexos['name']);
+    if($imagem[sizeof($imagem) -1] = '.jpeg' || $imagem[sizeof($imagem) -2] = '.png') {
+        move_uploaded_file($anexos['tmp_name'], 'imagens/'.$anexos['name']);
+
+    }else {
+        $mensagem = 'Esse arquivo não foi suportado pelo sistema.';
+        header('Location: CriarPost.php?mensagem=' . urldecode($mensagem));
+        exit();
+    }
+
+    // Verifica se tanto o título quanto o conteúdo não estão vazios e faz a inserção do post ao banco de dados
     if (!empty($titulo) && !empty($conteudo)) {
-        $sql = "INSERT INTO POSTS (TITULO, CONTEUDO, USER_ID) VALUES ('$titulo', '$conteudo', '$autor')";
+        $sql = "INSERT INTO POSTS (TITULO, CONTEUDO, ANEXOS, USER_ID) VALUES ('$titulo', '$conteudo', '".$anexos['name']."', '$autor')";
 
         if ($conn->query($sql) === TRUE) {
             $mensagem = "Post inserido com sucesso.";
-            header('location: ' . 'Home.php');
         } else {
             $mensagem = "Erro ao inserir post: " . $conn->error;
         }
     } else {
         $mensagem = "Por favor, preencha tanto o título quanto o conteúdo do post.";
     }
+    $conn->close();
+    header('location: ' . 'Home.php');
+    exit();
 }
-$conn->close();
