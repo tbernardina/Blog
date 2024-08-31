75a136cc07728a175223a2f0a87312bd6a4529f3
tbernardina <trdallabernardina@gmail.com>
2024-08-31 09:16:07 -0300

    teste data

--- ProcessarPost.php
+++ ProcessarPost.php
@@ -9,6 +9,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $conteudo = $_POST['conteudo'];
     $autor = $_SESSION['id'];
     $anexos = $_FILES['anexo'];
+    date_default_timezone_set('America/Sao_Paulo');
+    $data_publicacao = date('Y-m-d H:i:s');
 // Código de anexo de imagens
     if(isset($anexos) && !empty($anexos['name'])){
         $imagem = strtolower(pathinfo($anexos['name'], PATHINFO_EXTENSION)); //Captura o formato do anexo e verifica se está na lista de formatos válidos
@@ -22,8 +24,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 // Código de anexo de imagens
 // Código de insert dos posts criados
+
     if (!empty($titulo) && !empty($conteudo)) { // Verifica se tanto o título quanto o conteúdo não estão vazios e faz a inserção do post ao banco de dados
-        $sql = "INSERT INTO u210937242_posts (TITULO, CONTEUDO, ANEXOS, USER_ID, DATA_PUBLICACAO) VALUES ('$titulo', '$conteudo', '".$anexos['name']."', '$autor')"; // Query de insert das informações dos posts
+        $sql = "INSERT INTO u210937242_posts (TITULO, CONTEUDO, ANEXOS, USER_ID, DATA_PUBLICACAO) VALUES ('$titulo', '$conteudo', '".$anexos['name']."', '$autor', '$data_publicacao')"; // Query de insert das informações dos posts
         if ($conn->query($sql) === TRUE) {
             header('location: Home.php');
             exit();
--- conexao.php
+++ conexao.php
@@ -6,7 +6,6 @@ $password = "Thiagoreis2019_";
 $database = "u210937242_Projeto0";
 
 $conn = new mysqli($servername, $username, $password, $database);
-$conn->query("SET time_zone = 'America/Sao_Paulo'");
 
 if($conn->connect_error){
     die("Falha ao conectar ao banco de dados: " . $conn->connect_error);
