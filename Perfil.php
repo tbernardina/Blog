19aee595608596e9316d72860aaef6f8daa0ecf4
Thiago Reis Dalla Bernardina <trdallabernardina@gmail.com>
2024-11-28 15:56:37 -0300

    Atualização da página de perfil
    Atualizado a página de perfil puxando todos os atributos do usuário.

--- Perfil.php
+++ Perfil.php
@@ -3,64 +3,41 @@ include("start.php");
 include("conexao.php");
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php 
+    $select_perfil = "SELECT * FROM u210937242_usuario WHERE USER_ID =" . $_SESSION['id'];
+    $resultado = $conn->query($select_perfil) or die("Falha na execução do código SQL: " . $conn->connect_error) ;
+    $perfil = $resultado->fetch_assoc();
+    ?>
 </head>
 <body>
-    <main>
-        <!-- SEÇÃO DE POSTS -->
-        <section>
-            <h2>Últimas postagens</h2>
+    <section>
+        <div>
+            <label for="nome">Nome:</label>
+            <input id="nome" type="text" value='<?php echo $perfil['NOME']; ?>' disabled>
+            <label for="sobrenome">Sobrenome:</label>
+            <input id="nickname" type="text" value='<?php echo $perfil['SOBRENOME'];?>' disabled>
+            <label for="nickname">Usuário:</label>
+            <input id="nickname" type="text" placeholder='<?php echo $perfil['NICKNAME']; ?>'>
+            <label id="senha" for="senha">Senha:</label>
+            <input id="senha" type="text" placeholder="Senha">
+            <label id="confirmarSenha" for="senha">Confirmar senha:</label>
+            <input id="confirmarSenha" type="text" placeholder="Confirmar senha">
+        </div>
+        <div>
+            <label for="ImagemPerfil">Foto de Perfil:</label>
             <?php
-            $slct = "SELECT p.*, u.NOME, u.SOBRENOME FROM u210937242_posts p JOIN u210937242_usuario u ON p.USER_ID = u.USER_ID WHERE P.USER_ID = " . $_SESSION['id'];
-            $result = $conn->query($slct);
-
-            if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
+                $CaminhoImagem = "Imagens/" . $perfil['PERFIL_IMG'];
+                if (file_exists($CaminhoImagem)) {
+                    echo "<img class='ImagemPerfil' src='$CaminhoImagem' alt='Imagem'>";
+                }else{echo ' ';};
             ?>
-                    <article>
-                        <div class="TituloPost">
-                            <h3><?php echo $row['TITULO']; ?></h3>
-                            <div class="data"><?php echo $row['DATA_PUBLICACAO']; ?></div>
-                        </div>
-                        <div class="ImagemAnexo">
-                            <?php
-                            $CaminhoImagem = "Imagens/" . $row['ANEXOS'];
-                            if (file_exists($CaminhoImagem)) {
-                                echo "<img class='ImagemAnexos' src='$CaminhoImagem' alt='Imagem'>";
-                            }else{echo ' ';};
-                            ?>
-                        </div>
-                        <p><?php echo $row['CONTEUDO']; ?></p>
-                        <p>Autor: <?php echo $row['NOME'] . " " . $row['SOBRENOME']; ?></p>
-                        <form method="post" action="CriarComentario.php">
-                            <input type="hidden" name="postId" value="<?php echo $row['POST_ID']; ?>">
-                            <input type="text" name="conteudo_C" placeholder="Comente aqui" required>
-                            <input type="submit" value="Enviar">
-                        </form><br>
-                        <!-- SEÇÃO DE COMENTÁRIOS -->
-                        <ul class="comments" id="comments_<?php echo $row['POST_ID']; ?>">
-                            <?php
-                            $postId = $row['POST_ID'];
-                            $slct_coment = "SELECT c.*, u.NOME, u.SOBRENOME FROM u210937242_comentario c JOIN u210937242_usuario u ON c.USER_ID = u.USER_ID WHERE c.POST_ID = '$postId'";
-                            $result_coment = $conn->query($slct_coment);
-
-                            if ($result_coment->num_rows > 0) {
-                                while ($row_coment = $result_coment->fetch_assoc()) {
-                                    echo "<li>" . $row_coment['CONTEUDO'] . " - " . $row_coment['NOME'] . "</li>";
-                                }
-                            }
-                            ?>
-                        </ul>
-                    </article>
-            <?php
-                }
-            }
-            ?>
-        </section>
-    </main>
+            <input type="file">
+        </div>
+    </section>
 </body>
 </html>
