e7f9a61616f10a15c8dd4f73cba53fe928af3906
Thiago Reis Dalla Bernardina <trdallabernardina@gmail.com>
2025-03-21 14:39:33 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -10,3 +10,4 @@ $conn = new mysqli($servername, $username, $password, $database);
 if($conn->connect_error){
     die("Falha ao conectar ao banco de dados: " . $conn->connect_error);
 }
+//Comentário teste para verificação do script do servidor
